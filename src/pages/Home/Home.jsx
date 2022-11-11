4322c92813e59c5ef821dcc616b0e4cd32be894b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-11 10:06:56 +0700

    initialize-bas

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <h1 className='title'>BaoNguyen dev</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Single/Single.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './single.scss'
+
+const Single = () => {
+  return (
+    <div>Single</div>
+  )
+}
+
+export default Single
