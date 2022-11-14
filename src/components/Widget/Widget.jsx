2434a336f4c50cdfc6528b20e131ba87e9137a15
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-15 05:24:51 +0700

    update-home#3

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -4,7 +4,12 @@ import './widget.scss'
 const Widget = () => {
   return (
     <div className='widget'>
-        Widget
+        <div className="left">
+          <span className="title">USERS</span>
+          <span className="counter">21312</span>
+          <span className="link">See all user</span>
+        </div>
+        <div className="right">right</div>
     </div>
   )
 }
