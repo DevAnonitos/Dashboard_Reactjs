b46152e01aed62b0b2545dd158cfd4ccbcf09fba
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-16 22:59:14 +0700

    update-widget#3

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -10,15 +10,55 @@ const Widget = ({ type }) => {
 
   let data;
 
+  // temp
+  const amount = 100;
+  const diff = 20;
+
   switch(type){
     case 'user':
       data = {
         title: 'USERS',
         isMoney: false,
         link: "See all users",
-        icon: <PersonOutlineIcon className='icon' />
+        icon: <PersonOutlineIcon className='icon' style={{
+          color: 'crimson',
+          backgroundColor: 'rgba(255, 0, 0, 0.2',
+        }} />
       };
       break;
+    case 'order':
+        data = {
+          title: 'ORDERS',
+          isMoney: false,
+          link: "View all orders",
+          icon: <ShoppingCartOutlinedIcon className='icon' style={{
+            color: 'goldenrod',
+            backgroundColor: 'rgba(218, 165, 32, 0.2)',
+          }} />
+        };
+      break;
+    case 'earning':
+      data = {
+        title: 'EARNINGS',
+        isMoney: true,
+        link: "View net earning",
+        icon: <MonetizationOnOutlinedIcon className='icon' style={{
+          color: 'green',
+          backgroundColor: 'rgba(0, 128, 0, 0.2)',
+        }} />
+      };
+    break;
+    case 'balance':
+      data = {
+        title: 'BALANCE',
+        isMoney: true,
+        link: "See details",
+        icon: <AccountBalanceWalletOutlinedIcon className='icon' style={{
+          backgroundColor: 'rgba(128, 0, 128, 0.2)',
+          color: 'purple',
+        }} />
+      };
+    break;
     default:
       break;
   }
@@ -26,16 +66,16 @@ const Widget = ({ type }) => {
   return (
     <div className='widget'>
         <div className="left">
-          <span className="title">USERS</span>
-          <span className="counter">21312</span>
-          <span className="link">See all user</span>
+          <span className="title">{data.title}</span>
+          <span className="counter">{data.isMoney && "$"} {amount}</span>
+          <span className="link">{data.link}</span>
         </div>
         <div className="right">
           <div className="percentage positive">
             <KeyboardArrowUpIcon />
-            20 %
+            {diff} %
           </div>
-          <PersonOutlineIcon className='icon' />
+          {data.icon}
         </div>
     </div>
   )
