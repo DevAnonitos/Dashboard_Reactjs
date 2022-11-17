ab4674082ffcb451e1310acb9994f266502a21a2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-18 06:11:20 +0700

    update

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,6 +3,8 @@ import './featured.scss'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 const Featured = () => {
   return (
@@ -20,6 +22,29 @@ const Featured = () => {
         <p className="desc">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe quos fugiat voluptatum quae recusandae accusantiu
         </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
