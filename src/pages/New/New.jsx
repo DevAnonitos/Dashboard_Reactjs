1354530e3fd048ca39dc13798bd2a04ad4bfb01e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-26 23:47:16 +0700

    update-new@4

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -14,10 +14,23 @@ const New = () => {
         </div>
         <div className="bottom">
           <div className="left">
-            left
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="Logo" className='img-add' />
           </div>
           <div className="right">
-            right
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder='Mongosee' />
+              </div>
+              <div className="formInput">
+                <label>Name and surname</label>
+                <input type="text" placeholder='Db monge' />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder='Mongge2212@gmail.com' />
+              </div>
+            </form>
           </div>
         </div>
       </div>
