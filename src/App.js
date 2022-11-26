13b24d3211d97115c6078b7cfb7ccf73d0173afd
Nguyễn Đức Gia Bảo <91384216+NguyenBao23131@users.noreply.github.com>
2022-11-26 03:17:36 +0000

    srcclear

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 console.info(
   `\n%c⚠️ Warning ⚠️%c \n\n\n%cThis is likely not an issue with Dashy, but rather your configuration. If you think it is a bug, please open a ticket on GitHub: https://git.io/JukXk`,
-  "color:#ceb73f; background: #ceb73f33; font-size:1.5rem; padding:0.15rem; margin: 1rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #ceb73f; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
+  "color:#ceb73f; background: #f9ca24; font-size:1.5rem; padding:0.15rem; margin: 1rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #ceb73f; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
   'font-weight: bold; font-size: 1rem;color: #ceb73f;',
   "color: #ceb73f; font-size: 0.75rem; font-family: Tahoma, 'Trebuchet MS', Helvetica;",
 );
