5844508379a8a207423e2080f1f65ff4744f9bec
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-27 10:41:05 +0700

    update-scssNew@1

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './new.scss';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Navbar from '../../components/Navbar/Navbar'
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
 const New = () => {
   return (
@@ -18,6 +19,12 @@ const New = () => {
           </div>
           <div className="right">
             <form>
+            <div className="formInput">
+                <label htmlFor='file'>
+                  Image: <DriveFolderUploadIcon className='icon' />
+                </label>
+                <input type="file" id='file' style={{display: 'none'}}/>
+              </div>
               <div className="formInput">
                 <label>Username</label>
                 <input type="text" placeholder='Mongosee' />
@@ -30,6 +37,23 @@ const New = () => {
                 <label>Email</label>
                 <input type="email" placeholder='Mongge2212@gmail.com' />
               </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="email" placeholder='+84 910341353' />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder='St.419 Forallnight Yd.VietNam' />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder='VietNam' />
+              </div>
+              <button className='btn-send'>Send</button>
             </form>
           </div>
         </div>
